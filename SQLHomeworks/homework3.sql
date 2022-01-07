1d8660d4f9a6f960e482a95d7fe0d1897709f003
Mert Yalçın <82022374+mertyalcin-code@users.noreply.github.com>
2022-01-07 15:56:29 +0300

    new Project

---
+++ SQLHomeworks/homework3.sql
@@ -0,0 +1,9 @@
+select * from country where country like 'A%a';
+select * from country where country like '_____%n';
+select title from film where title ilike '%T%%T%%T%%T%'
+select * from film where title like 'C%' and film.length>90 and rental_rate=2.99;
+/*
+country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.*/
