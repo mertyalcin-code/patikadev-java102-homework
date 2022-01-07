e81f7c1a7e325150203a9481109ad37e3a4772b1
Mert Yalçın <82022374+mertyalcin-code@users.noreply.github.com>
2022-01-07 16:32:37 +0300

    new Project

---
+++ SQLHomeworks/homework5.sql
@@ -0,0 +1,8 @@
+select * from film where title like '%n' order by film.length desc limit 5;
+select * from film where title like '%n' order by film.length asc offset 5 limit 5;
+select * from customer where store_id=1 order by last_name desc limit 4;
+/*
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+*/
